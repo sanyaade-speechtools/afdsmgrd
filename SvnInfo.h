1c6617972c3f789660ccd7e2a9ce5d5d39c7d1c9
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-02 15:41:44 +0000

    * Test svn props

---
+++ SvnInfo.h
@@ -0,0 +1,15 @@
+/* SVN FILE: $Id$ */
+/**
+* Project Name : Project Description
+*
+* @package className
+* @subpackage subclassName
+* @author $Author$
+* @copyright $Copyright$
+* @version $Revision$
+* @lastrevision $Date$
+* @modifiedby $LastChangedBy$
+* @lastmodified $LastChangedDate$
+* @license $License$
+* @filesource $URL$
+*/
