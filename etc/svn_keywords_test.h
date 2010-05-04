232a82f3adb7df2327b99e2e42fa17082ea21970
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-04 09:38:10 +0000

    * SVN keywords test, part 2

--- etc/svn_keywords_test.h
+++ etc/svn_keywords_test.h
@@ -1,5 +1,7 @@
 /** Test of SVN keywords substitution:
+ * --A new line added: the line below said 61 2010-05-04 09:36:30Z dario.berzano...
  * Id: $Id$
  * Revision: $Revision$
+ * --Here there is also a new line added
  * Date: $Date$
  */
