061c998c1b665b1fa087e0deb2083d8b126f2e94
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-04 13:27:44 +0000

    * Created tagged revision v0.1.1

---
+++ tags/v0.1.1/src/AfDataSetsManager.h
@@ -0,0 +1,59 @@
+#ifndef AFDATASETSMANAGER_H
+#define AFDATASETSMANAGER_H
+
+#include "AfConfReader.h"
+#include "AfDataSetSrc.h"
+#include "AfStageUrl.h"
+#include "AfLog.h"
+
+#include <TThread.h>
+#include <TObjArray.h>
+#include <TObjString.h>
+
+class AfDataSetsManager {
+
+  public:
+
+    AfDataSetsManager();
+    ~AfDataSetsManager();
+    Int_t  ProcessAllDataSetsOnce(DsAction_t action);
+    void   Loop();
+    void   Reset();
+    Bool_t ReadConf(const char *cf);
+    void   SetSuid(bool suid = kTRUE) { fSuid = suid; }
+
+    StgStatus_t GetStageStatus(const char *url);
+    Bool_t EnqueueUrl(const char *url);
+    Bool_t DequeueUrl(const char *url);
+    void ProcessTransferQueue();
+    void PrintStageList(const char *header, Bool_t debug);
+
+  private:
+
+    // Private methods
+    static void *Stage(void *args);
+
+    // Private variables
+    static const Int_t  kDefaultParallelXfrs = 1;
+    static const Int_t  kDefaultLoopSleep_s = 3600;
+    static const Int_t  kDefaultScanDsEvery = 10;
+
+    TList              *fSrcList;
+    Bool_t              fSuid;
+    TString             fRedirHost;
+    UShort_t            fRedirPort;
+    Int_t               fLoopSleep_s;
+    Int_t               fScanDsEvery;
+    TString             fStageCmd;
+    Int_t               fParallelXfrs;
+
+    TList              *fStageQueue;
+    TList              *fStageCmds;
+
+    Int_t               fLastQueue;
+    Int_t               fLastStaging;
+    Int_t               fLastFail;
+    Int_t               fLastDone;
+};
+
+#endif // AFDATASETSMANAGER_H
