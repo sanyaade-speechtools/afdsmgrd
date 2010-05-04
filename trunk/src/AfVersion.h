d3f7941e61eed909301b3e3f7c4280a1bb882b71
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-04 10:04:46 +0000

    * Small changement in AfVersion

--- trunk/src/AfVersion.h
+++ trunk/src/AfVersion.h
@@ -1,3 +1,3 @@
 // Version number is MAJOR.MINOR.PATCHES (or "trunk")
-// Guidelines followed are here: http://apr.apache.org/versioning.html
+// Followed guidelines are here: http://apr.apache.org/versioning.html
 #define AFVER "trunk"
