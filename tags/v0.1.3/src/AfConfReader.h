b31fb66cfb270ed2282edac906bc5ba40d40b0db
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-06 08:33:49 +0000

    * Restored a tag

---
+++ tags/v0.1.3/src/AfConfReader.h
@@ -0,0 +1,37 @@
+#ifndef AFCONFREADER_H
+#define AFCONFREADER_H
+
+#include "AfLog.h"
+
+#include <Riostream.h>
+#include <TPRegexp.h>
+#include <TList.h>
+#include <TObjString.h>
+#include <TMap.h>
+
+class AfConfReader {
+
+  public:
+
+    ~AfConfReader();
+    TString *GetVar(const char *vn);
+    TString *GetDir(const char *dn);
+    TList *GetDirs(const char *dn);
+    static AfConfReader *Open(const char *cffn, Bool_t subVars = kTRUE);
+    void PrintVarsAndDirs();
+
+  private:
+
+    // Private methods
+    AfConfReader();  // constructor is private
+    void SubstVar();
+    void ReadConf();
+
+    // Private variables
+    ifstream    fFp;
+    TList     *fDirs;
+    TMap      *fVars;
+
+};
+
+#endif // AFCONFREADER_H
