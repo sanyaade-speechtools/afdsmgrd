29a488f20ff7caf4a02eaef884af0bc5fc0fd023
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-05-28 18:22:50 +0000

    * Macro for repairing files added (preliminary), see https://savannah.cern.ch/bugs/?68011  * Several other changes made in the macros

--- trunk/macros/afdsutil.C
+++ trunk/macros/afdsutil.C
@@ -1,48 +1,139 @@
+/** Init function, it only prints a welcome message with the default parameters.
+ */
 void afdsutil() {
-  Printf("Remember to connect to PROOF!");
+  const char *dsPath = gEnv->GetValue("af.dspath", "/tmp");
+  Printf("Dataset management utilities loaded.");
+  Printf("Available functions start with \"af\", use autocompletion to list");
+  Printf(">> Datasets path, change with afSetDsPath(): %s", dsPath);
 }
 
-void afds_list(const char *uri) {
+/** Sets the path of the dataset source, and saves it to the af.dspath variable
+ *  inside the user's ~/.rootrc.
+ */
+void afSetDsPath(const char *dsPath) {
+  gEnv->SetValue("af.dspath", gDsPath.Data());
+  gEnv->SaveLevel(kEnvUser);
+}
 
-  TFileCollection *fc = gProof->GetDataSet(uri);
+/** A TDataSetManagerFile object is created using default parameters.
+ */
+TDataSetManagerFile *afCreateDsMgr() {
+  TDataSetManagerFile *mgr = new TDataSetManagerFile( NULL, NULL,
+    Form("dir:%s", gEnv->GetValue("af.dspath", "/pool/datasets")) );
+  return mgr;
+}
+
+/** A content of a dataset is shown. There is the possibility to show only files
+ *  that are (un)staged or (un)corrupted by combining one or more of SsCc in the
+ *  showOnly string parameter in any order.
+ *
+ *  Usage examples:
+ *
+ *   - To show every staged file:                  "S" or "SCc"
+ *   - To show every corrupted file:               "C" or "CSs"
+ *   - To show every unstaged and corrupted file:  "sC"
+ *   - To show every file (default):               "Ss" or "Cc" or "SsCc"
+ */
+void afShowDsContent(const char *dsUri, TString showOnly = "SsCc") {
+
+  TDataSetManagerFile *mgr = afCreateDsMgr();
+  TFileCollection *fc = mgr->GetDataSet(dsUri);
 
   if (!fc) {
-    Printf("Couldn't open dataset URI %s", uri);
+    Printf("Error opening dataset URI %s", dsUri);
     return;
   }
 
+  Bool_t bS, bs, bC, bc;
+  bS = bs = bC = bc = kFALSE;
+
+  if (showOnly.Index('S') >= 0) bS = kTRUE;
+  if (showOnly.Index('s') >= 0) bs = kTRUE;
+  if (showOnly.Index('C') >= 0) bC = kTRUE;
+  if (showOnly.Index('c') >= 0) bc = kTRUE;
+
+  // If Ss (or Cc) omitted, show both Ss (or Cc)
+  if (!bc && !bC) bc = bC = kTRUE;
+  if (!bs && !bS) bs = bS = kTRUE;
+
   TIter i(fc->GetList());
   TFileInfo *inf;
+  UInt_t count = 1;
 
   while ( inf = dynamic_cast<TFileInfo *>(i.Next()) ) {
     Bool_t s = inf->TestBit(TFileInfo::kStaged);
     Bool_t c = inf->TestBit(TFileInfo::kCorrupted);
-    Printf("%c%c | %s", (s ? 'S' : 's'), (c ? 'C' : 'c'),
-      inf->GetCurrentUrl()->GetUrl());
-    TUrl *url;
-    inf->NextUrl();
-    while (url = inf->NextUrl()) {
-      Printf("   | %s", url->GetUrl());
+
+    if ( ((s && bS) || (!s && bs)) && ((c && bC) || (!c && bc)) ) {
+      Printf("% 4u. %c%c | %s", count++, (s ? 'S' : 's'), (c ? 'C' : 'c'),
+        inf->GetCurrentUrl()->GetUrl());
+      TUrl *url;
+      inf->NextUrl();
+
+      // Every URL is shown, not only first
+      while (url = inf->NextUrl()) {
+        Printf("         | %s", url->GetUrl());
+      }
+      inf->ResetUrl();
     }
-    inf->ResetUrl();
   }
 
   delete fc;
+  delete mgr;
 }
 
-void afds_mark(const char *fileUrl, TString bits = "") {
+/** Gets a flattened TList of datasets. The list must be destroyed by the user,
+ *  and it is owner of its content.
+ */
+TList *afGetListOfDs(const char *mask = "/*/*") {
 
-  // Due to PROOF limitations, only datasets from /GROUP/USER are allowed. We
-  // also note that GetDataSets() returns COMMON substitutions if commonuser and
-  // commongroup are set in the groupfile, but RegisterDataSet does not support
-  // substitutions from COMMON back to the original user and group... bug or
-  // feature?
-  // A possible option to solve this kind of problems is to manually instantiate
-  // a TDataSetManager, but this ROOT session should be run on the master
-  // (because datasets are "local" wrt the master), and from a privileged user
-  // (because we need to write back saved datasets).
-  Printf("*** Searching only in datasets under /%s/%s ***", gProof->GetGroup(),
-    gProof->GetUser());
+  TDataSetManagerFile *mgr = afCreateDsMgr();
+  TList *listOfDs = new TList();
+  listOfDs->SetOwner();
+
+  TMap *groups = mgr->GetDataSets(mask, TDataSetManager::kReadShort);
+  groups->SetOwnerKeyValue();  // important to avoid leaks!
+  TIter gi(groups);
+  TObjString *gn;
+
+  while (gn = dynamic_cast<TObjString *>(gi.Next())) {
+
+    TMap *users = dynamic_cast<TMap *>( groups->GetValue( gn->String() ) );
+    users->SetOwnerKeyValue();
+    TIter ui(users);
+    TObjString *un;
+
+    while (un = dynamic_cast<TObjString *>(ui.Next())) {
+
+      TMap *dss = dynamic_cast<TMap *>( users->GetValue( un->String() ) );
+      dss->SetOwnerKeyValue();
+      TIter di(dss);
+      TObjString *dn;
+
+      while (dn = dynamic_cast<TObjString *>(di.Next())) {
+
+        // No COMMON user/group mapping is done here...
+        TString dsUri = TDataSetManager::CreateUri( gn->String(), un->String(),
+          dn->String() );
+
+        listOfDs->Add( new TObjString(dsUri.Data()) );
+
+      }
+    }
+  }
+
+  delete groups;
+  delete mgr;
+
+  return listOfDs;
+}
+
+/** Marks a file matching the URL as (un)staged or (un)corrupted: choose mode
+ *  with one or more among "SsCc". Clearly, "S" is incompatible with "s" and "C"
+ *  is incompatible with "c". The URL may match any URL of a TFileInfo, even not
+ *  the first one.
+ */
+void afMarkUrlAs(const char *fileUrl, TString bits = "") {
 
   Bool_t setStaged = kFALSE;
   Bool_t unsetStaged = kFALSE;
@@ -87,26 +178,24 @@ void afds_mark(const char *fileUrl, TString bits = "") {
     return;
   }
 
+  TDataSetManagerFile *mgr = afCreateDsMgr();
+  TList *listOfDs = afGetListOfDs();
   Int_t regErrors = 0;
+  TIter i(listOfDs);
+  TObjString *dsUriObj;
 
-  TMap *uris = gProof->GetDataSets(Form("/%s/%s", gProof->GetGroup(),
-    gProof->GetUser()));
-  TIter i(uris);
-  TObjString *u;
+  while (dsUriObj = dynamic_cast<TObjString *>(i.Next())) {
 
-  while (u = dynamic_cast<TObjString *>(i.Next())) {
+    TString dsUri = dsUriObj->GetString();
+    Int_t nChanged = 0;
 
-    TString dsName = u->GetString();
-
-    TFileCollection *fc = gProof->GetDataSet(dsName.Data());
+    TFileCollection *fc = mgr->GetDataSet(dsUri.Data());
     TIter j(fc->GetList());
     TFileInfo *fi;
 
-    Int_t nChanged = 0;
-
     while (fi = dynamic_cast<TFileInfo *>(j.Next())) {
       if ( fi->FindByUrl(fileUrl) ) {
-        Printf(">> Found on dataset %s", dsName.Data());
+        Printf(">> Found on dataset %s", dsUri.Data());
 
         if (setCorr) {
           fi->SetBit(TFileInfo::kCorrupted);
@@ -129,9 +218,14 @@ void afds_mark(const char *fileUrl, TString bits = "") {
     if (nChanged > 0) {
       fc->Update();
       // O = overwrite existing dataset
-      // T = trust information (i.e. don't reset STAGED and CORRUPTED bits): the
-      //     dataset manager must be configured to allow so (it is by default)
-      if (!gProof->RegisterDataSet(dsName.Data(), fc, "OT")) {
+      // T = trust information (i.e. don't reset STAGED and CORRUPTED bits):
+      //     the dataset manager must be configured to allow so (it is by
+      //     default on PROOF)
+      TString group, user, dsName;
+      mgr->ParseUri(dsUri, &group, &user, &dsName);
+      Int_t r = mgr->WriteDataSet(group, user, dsName, fc);
+
+      if (r == 0) {
         regErrors++;
       }
     }
@@ -139,13 +233,142 @@ void afds_mark(const char *fileUrl, TString bits = "") {
     delete fc;
   }
 
-  delete uris;
+  delete mgr;
+  delete listOfDs;
 
   if (regErrors > 0) {
-    Printf("%d error(s) doing RegisterDataSet() encountered, check permissions",
+    Printf("%d error(s) writing back datasets encountered, check permissions",
       regErrors);
   }
   else {
     Printf("Success, no errors");
   }
+}
+
+/** Repair datasets: this function gives the possibility to take actions on
+ *  corrupted files. Possible actions are:
+ *
+ *   - ignore:  corrupted files are just listed
+ *   - unlist:  they are removed from dataset
+ *   - unstage: they are removed from dataset and they are deleted from storage
+ *   - uncorrupt: files are marked as uncorrupted (and unstaged)
+ *
+ *  TODO:
+ *
+ *   - implement unstage
+ *   - save the list of "bad" files somewhere (by removing duplicates)
+ */
+void afRepairDs(TString action = "ignore") {
+
+  // What to do?
+  char a;
+
+  if (action == "uncorrupt") {
+    a = 'C';
+  }
+  else if (action == "unstage") {
+    a = 'S';
+  }
+  else if (action == "unlist") {
+    a = 'L';
+  }
+  else if (action == "ignore") {
+    a = 'I';
+  }
+  else {
+    Printf("Action may only be one of uncorrupt, unstage, unlist or ignore.");
+    return;
+  }
+
+  TDataSetManagerFile *mgr = afCreateDsMgr();
+  TList *listOfDs = afGetListOfDs();
+  TObjString *dsUriObj;
+  TIter i(listOfDs);
+
+  // Loop over datasets
+  while (dsUriObj = dynamic_cast<TObjString *>(i.Next())) {
+
+    TString dsUri = dsUriObj->GetString();
+    TFileCollection *fc = mgr->GetDataSet(dsUri.Data());
+    TFileCollection *newFc;
+    
+    if (a != 'I') {
+      newFc = new TFileCollection();
+      newFc->SetDefaultTreeName( fc->GetDefaultTreeName() );
+    }
+
+    Printf("Scanning dataset %s for corrupted files...", dsUri.Data());
+
+    TIter j(fc->GetList());
+    TFileInfo *fi;
+    Int_t nChanged = 0;
+
+    while (fi = dynamic_cast<TFileInfo *>(j.Next())) {
+
+      Bool_t c = fi->TestBit(TFileInfo::kCorrupted);
+
+      if (c) {
+
+        if (a == 'C') {
+          Printf(">> Marking as uncorrupted: %s", fi->GetFirstUrl()->GetUrl());
+          fi->ResetBit(TFileInfo::kCorrupted);
+          fi->ResetBit(TFileInfo::kStaged);
+          TFileInfo *newFi = new TFileInfo(*fi);
+          newFc->Add(newFi);
+          nChanged++;
+        }
+        else if (a == 'L') {
+          Printf(">> Unlisting: %s", fi->GetFirstUrl()->GetUrl());
+          nChanged++;
+        }
+        else if (a == 'S') {
+          Printf(">> Unstaging (not yet implemented): %s",
+            fi->GetFirstUrl()->GetUrl());
+          nChanged++;
+        }
+        else {
+          Printf(">> Corrupted: %s", fi->GetFirstUrl()->GetUrl());
+        }
+
+      }
+      else if (a != 'I') {
+        // If file is good, put it in the new TFileCollection
+        TFileInfo *newFi = new TFileInfo(*fi);
+        newFc->Add(newFi);
+      }
+
+    }
+
+    delete fc;
+
+    if (nChanged > 0) {
+
+      newFc->Update();
+
+      TString group, user, dsName;
+      mgr->ParseUri(dsUri, &group, &user, &dsName);
+      Int_t r = mgr->WriteDataSet(group, user, dsName, newFc);
+
+      if (r != 0) {
+        Printf("");
+        Printf("*** The dataset %s has effectively changed! ***", dsUri.Data());
+        Printf(">> Default tree: %s", newFc->GetDefaultTreeName());
+        Printf(">> Number of files: %ld (%.2f%% staged)", newFc->GetNFiles(),
+          newFc->GetStagedPercentage());
+      }
+      else {
+        Printf("*** Can't write back modified dataset %s! ***", dsUri.Data());
+      }
+
+      Printf("");
+    }
+
+    if (a != 'I') {
+      delete newFc;
+    }
+
+  }
+
+  delete mgr;
+  delete listOfDs;
 }
