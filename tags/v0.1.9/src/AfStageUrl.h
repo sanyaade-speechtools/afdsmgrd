dd0433a5c182c24a95df10113c7c52825a5ac465
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-07-06 15:34:53 +0000

    * Created tagged revision v0.1.9

---
+++ tags/v0.1.9/src/AfStageUrl.h
@@ -0,0 +1,39 @@
+#ifndef AFSTAGEURL_H
+#define AFSTAGEURL_H
+
+#include "AfConfigure.h"
+
+#include <TUrl.h>
+
+typedef enum { kStgDone = 'D', kStgFail = 'F', kStgQueue = 'Q',
+  kStgStaging = 'S', kStgAbsent = 'A', kStgQueueFull = 'X' } StgStatus_t;
+
+class AfStageUrl : public TObject {
+
+  public:
+
+    AfStageUrl(const char *url, StgStatus_t status = kStgQueue);
+    StgStatus_t GetStageStatus() { return fStgStatus; };
+    void SetStageStatus(StgStatus_t status) { fStgStatus = status; };
+    virtual ULong_t Hash() const;
+    const TUrl *GetUrlObj() { return fUrl; }
+    TString GetUrl() const { return fUrl->GetUrl(); };
+    virtual Bool_t IsEqual(const TObject* obj) const;
+    virtual void ResetFailures() { fFailures = 0; };
+    virtual Int_t IncreaseFailures() { return ++fFailures; };
+    virtual Int_t GetFailures() { return fFailures; };
+    virtual ~AfStageUrl();
+
+  private:
+
+    // Private variables
+    StgStatus_t  fStgStatus;
+    TUrl        *fUrl;
+    UInt_t       fStrHash;
+    UInt_t       fFailures;
+
+  // ROOT stuff
+  ClassDef(AfStageUrl, 1);
+};
+
+#endif // AFSTAGEURL_H
