4dae7435364bc44a0e374854469ac10f82985e8c
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-07-07 18:08:47 +0000

    * Created tagged revision v0.2.1

---
+++ tags/v0.2.1/src/AfDataSetSrc.h
@@ -0,0 +1,62 @@
+#ifndef AFDATASETSRC_H
+#define AFDATASETSRC_H
+
+typedef enum { kDsReset, kDsProcess } DsAction_t;
+
+#include "AfConfigure.h"
+#include "AfLog.h"
+#include "AfDataSetsManager.h"
+
+#include <THashList.h>
+#include <TDataSetManagerFile.h>
+#include <TFileStager.h>
+#include <TFileCollection.h>
+#include <TFileInfo.h>
+#include <TUrl.h>
+#include <TFile.h>
+#include <TKey.h>
+#include <TTree.h>
+
+// Reciprocal inclusions between AfDataSetSrc and AfDataSetsManager require the
+// following forward declaration
+class AfDataSetsManager;
+
+class AfDataSetSrc : public TObject {
+
+  public:
+
+    AfDataSetSrc();
+    AfDataSetSrc(const char *url, TUrl *redirUrl, const char *opts,
+      AfDataSetsManager *parentManager);
+    Int_t Process(DsAction_t action);
+    void SetDsProcessList(TList *dsList);
+    ~AfDataSetSrc();
+
+  private:
+
+    // URL type enum (implicit conversion to Int_t)
+    typedef enum { kUrlRoot = 1, kUrlAliEn = 2 } UrlType_t;
+
+    // Private methods
+    void   FlattenListOfDataSets();
+    Int_t  ProcessDataSet(const char *uri);
+    Int_t  ResetDataSet(const char *uri);
+    void   ListDataSetContent(const char *uri, const char *header, Bool_t debug);
+    Int_t  TranslateUrl(TFileInfo *ti, Int_t whichUrls = kUrlRoot | kUrlAliEn);
+    Int_t  KeepOnlyLastUrl(TFileInfo *fi);
+    Bool_t AddRealUrlAndMetaData(TFileInfo *fi);
+
+    // Private variables
+    TDataSetManagerFile *fManager;
+    TList               *fDsUris;
+    TUrl                *fRedirUrl;
+    TString              fUrl;
+    TString              fOpts;
+    TList               *fDsToProcess;
+    AfDataSetsManager   *fParentManager;  //!
+
+  // ROOT stuff
+  ClassDef(AfDataSetSrc, 1);
+};
+
+#endif // AFDATASETSRC_H
