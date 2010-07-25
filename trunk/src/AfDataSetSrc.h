1984adea4f1af5e6b423e321fef11a301f40f0ab
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-07-25 20:23:21 +0000

    * BIG UPDATE: added specific actions for each dataset source!  * To fit with the needs of the dynamic facilities, now it is possible, with actions, to implement fully-dynamic datasets, without touching the PROOF code  * Everything is moved inside a file specific for each dataset source: the file is re-read at each loop, so there is no need to restart the daemon to make changes  * See the provided examples inside the etc/xrootd/ directory

--- trunk/src/AfDataSetSrc.h
+++ trunk/src/AfDataSetSrc.h
@@ -30,8 +30,8 @@ class AfDsUri : public TObject {
   public:
     AfDsUri() : fUri(""), fUId(0x0) {}
     AfDsUri(const char *uri, UInt_t uid = 0x0) : fUri(uri), fUId(uid) {};
-    virtual const char *GetUri() { return fUri.Data(); }
-    virtual UInt_t GetUId() { return fUId; }
+    virtual const char *GetUri() const { return fUri.Data(); }
+    virtual UInt_t GetUId() const { return fUId; }
     virtual Bool_t IsEqual(const TObject *r) const {
       return (fUri == dynamic_cast<const AfDsUri *>(r)->fUri);
     }
@@ -42,13 +42,38 @@ class AfDsUri : public TObject {
 
 };
 
+// Helper class that wraps a PERL regexp into a TObject, so that it becomes
+// collectable in a TList
+class AfDsMatch : public TObject {
+
+  public:
+    AfDsMatch() : fRegEx(NULL), fStr("") {}
+    AfDsMatch(const TString &s, Bool_t isRe) : fStr(s) {
+      if (isRe) fRegEx = new TPMERegexp(s);
+      else fRegEx = NULL;
+    }
+    virtual ~AfDsMatch() { if (fRegEx) delete fRegEx; }
+    TPMERegexp &GetRegEx() const { return *fRegEx; }
+    const TString &GetPattern() const { return fStr; }
+    Bool_t IsRegEx() const { return (fRegEx != NULL); }
+    Bool_t Match(const char *s) const {
+      if (fRegEx) return (fRegEx->Match(s) > 0);
+      return (s == fStr);
+    }
+
+  private:
+    TPMERegexp *fRegEx;
+    TString    fStr;
+
+};
+
 class AfDataSetSrc : public TObject {
 
   public:
 
     AfDataSetSrc();
     AfDataSetSrc(const char *url, TUrl *redirUrl, const char *opts,
-      const char *syncUrl, AfDataSetsManager *parentManager);
+      const char *dsConf, AfDataSetsManager *parentManager);
     Int_t Process(DsAction_t action);
     void SetDsProcessList(TList *dsList);
     ~AfDataSetSrc();
@@ -57,16 +82,24 @@ class AfDataSetSrc : public TObject {
 
     // URL type enum (implicit conversion to Int_t)
     typedef enum { kUrlRoot = 1, kUrlAliEn = 2 } UrlType_t;
+    enum { kActIgnore = (1 << 4) - 1, kActTranslate = (1 << 3),
+      kActStage = (1 << 2), kActVerify = (1 << 1), kActAddEndUrl = 1 };
 
     // Private methods
     void   FlattenListOfDataSets();
+    void   ReadDsActionsConf();
+    UInt_t ParseActionsToBits(TString &act, TString *name = NULL);
     Int_t  ProcessDataSet(AfDsUri *dsUri);
     Int_t  ResetDataSet(const char *uri);
     Int_t  Sync();
     void   ListDataSetContent(const char *uri, const char *header, Bool_t debug);
     Int_t  TranslateUrl(TFileInfo *ti, Int_t whichUrls = kUrlRoot | kUrlAliEn);
     Int_t  KeepOnlyLastUrl(TFileInfo *fi);
-    Bool_t AddRealUrlAndMetaData(TFileInfo *fi);
+    Bool_t AddRealUrlAndMetaData(TFileInfo *fi, Bool_t addEndUrl,
+      Bool_t addMeta);
+    AfDsMatch *GetActionsForDs(AfDsUri *dsUri);
+    static Bool_t AliEnConnect();
+
 
     // Private variables
     TDataSetManagerFile *fManager;
@@ -75,7 +108,10 @@ class AfDataSetSrc : public TObject {
     TString              fUrl;
     TString              fOpts;
     TUrl                 fSyncUrl;
+    TString              fActionsConf;
     TList               *fDsToProcess;
+    TList               *fActions;
+    AfDsMatch            fDefaultAction;
     AfDataSetsManager   *fParentManager;  //!
 
   // ROOT stuff
