f0a243e94392a78d756a883863b95fa356bca4fa
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2010-09-08 11:06:02 +0000

    * ApMon is searched in the right path when installing with aaf-installer  * make install copies also the example configuration for the dataset source

--- trunk/cmake/modules/FindApMon.cmake
+++ trunk/cmake/modules/FindApMon.cmake
@@ -13,11 +13,13 @@ set (ALIEN_DIR $ENV{ALIEN_DIR})
 find_path (ApMon_INCDIR
   ApMon.h
   PATHS ${ApMon_PREFIX}/include ${ALIEN_DIR}/api/include ${ALIEN_DIR}/include
+  NO_DEFAULT_PATH
 )
 
 find_library (ApMon_LIBRARY
   apmoncpp
   PATHS ${ApMon_PREFIX}/lib ${ALIEN_DIR}/api/lib ${ALIEN_DIR}/api/lib64 ${ALIEN_DIR}/lib ${ALIEN_DIR}/lib64
+  NO_DEFAULT_PATH
 )
 
 if ((NOT ApMon_INCDIR) OR (NOT ApMon_LIBRARY))
