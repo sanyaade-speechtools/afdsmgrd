1008830ea77cff231a55ff12d2ae20a23ff37c57
dario.berzano <dario.berzano@67fda8f4-9fcb-5d7f-09a7-449be58e81b0>
2011-03-25 18:06:26 +0000

    * In AFDSUtil: added AOD example in CreateDataSetFromAliEn

--- trunk/macros/CreateDataSetFromAliEn.C
+++ trunk/macros/CreateDataSetFromAliEn.C
@@ -17,22 +17,39 @@ void CreateDataSetFromAliEn(
   // Customize with your user and your group (optional)
   TString connStr   = "youruser:default@alice-caf.cern.ch",
 
-  // Example for official real data: the <RUN9> string in basePath means to
-  // insert the run number, zero-padded to 9 digits. In dsPattern, <RUN> is
-  // zero-padded to 9 digits by default. Keep in mind that basePath and fileName
-  // are in fact the first two parameters of AliEn find, thus they support basic
-  // jolly characters expansion features. The '*' is used as jolly character.
+  // Example for ESDs from official real data: the <RUN9> string in basePath
+  // means to insert the run number, zero-padded to 9 digits. In dsPattern,
+  // <RUN> is zero-padded to 9 digits by default. Keep in mind that basePath and
+  // fileName are in fact the first two parameters of AliEn find, thus they
+  // support basic jolly characters expansion features. The '*' is used as jolly
+  // character
   TString basePath  = "/alice/data/2010/LHC10h/<RUN9>/ESDs_lowflux/pass1/*.*",
   TString fileName  = "root_archive.zip",
   TString filter    = "",  // no filter after AliEn find is used
   TString anchor    = "AliESDs.root",
   TString treeName  = "/esdTree",
   TString runList   = "136837,137045",
-  TString dsPattern = "LHC10h_<RUN>",
+  TString dsPattern = "LHC10h_<RUN>_ESDs_lowflux",
 
-  // Example for official Monte Carlo. In this example, filter is used to prune
-  // AOD and QA directories (just try AliEn find with and without that), and
-  // run number is zero-padded to 6 digits
+  // Example for all AODs from official real data: see above for an explanation
+  // on format strings. This example makes use of a more complex regular
+  // expression to filter all the entries under basePath which contain in their
+  // name a directory "AOD" followed by three numbers and a subdirectory whose
+  // name is a number with 3 or 4 digits. This example also shows how to specify
+  // a run range
+  /*
+  TString basePath  = "/alice/data/2010/LHC10h/<RUN9>/ESDs/pass1",
+  TString fileName  = "root_archive.zip",
+  TString filter    = "AOD[0-9]{3}/[0-9]{3,4}",  // get only AODnnn/mmmm dirs
+  TString anchor    = "AliAOD.root",  // *not* AliAODs.root!
+  TString treeName  = "/aodTree",
+  TString runList   = "139104-139107,139306",
+  TString dsPattern = "LHC10h_<RUN>_AllAODs",
+  */
+
+  // Example for ESDs from official Monte Carlo. In this example, filter is used
+  // to prune AOD and QA directories (just try AliEn find with and without
+  // that), and run number is zero-padded to 6 digits
   /*
   TString basePath  = "/alice/sim/LHC10c9/<RUN6>",
   TString fileName  = "root_archive.zip",
@@ -40,7 +57,7 @@ void CreateDataSetFromAliEn(
   TString anchor    = "AliESDs.root",
   TString treeName  = "/esdTree",
   TString runList   = "115315,119846",
-  TString dsPattern = "LHC10c9_<RUN>",
+  TString dsPattern = "MC_LHC10c9_<RUN>_ESDs",
   */
 
   // Host name (and, optional, port) of redirector -- leave empty "" if it is
